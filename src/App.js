63714a4e07556ee62339c822204b37dc32c52b9e
Hunter5555 <hunter.m.allan@gmail.com>
2019-11-06 16:49:46 -0700

    app.js changes primairly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+
 
 function App() {
+  const [nasaImg, setImg] = useState(0);
+  
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const nasaData = await axios.get('https://api.nasa.gov/planetary/apod?api_key=NAiBVbBWUEMwQapNNOSb7drcvy0f0vTdYpF1aRmP')
+        console.log(nasaData);
+        setImg(nasaData.data);
+      }
+
+
+
   return (
     <div className="App">
       <p>
