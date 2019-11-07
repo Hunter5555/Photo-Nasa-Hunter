09ef0a3e25d9c85fd6224d2e6eca8633ccccdb99
Hunter5555 <hunter.m.allan@gmail.com>
2019-11-06 17:02:15 -0700

    adding styles

--- src/App.js
+++ src/App.js
@@ -13,15 +13,20 @@ function App() {
         console.log(nasaData);
         setImg(nasaData.data);
       }
-
-
+        catch (err){
+          console.log(err);
+        }
+      }
+      fetchData()
+    },[])
+    console.log(nasaImg);
 
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h2>{nasaImg.title}</h2>
+      <h2>{nasaImg.date}</h2>
+      <p>{nasaImg.explanation}</p>
+      <img src={`${nasaImg.url}`} />
     </div>
   );
 }
