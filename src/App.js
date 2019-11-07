9e057961f9535c22ff7396154a670deab9e50c82
Hunter5555 <hunter.m.allan@gmail.com>
2019-11-06 17:26:30 -0700

    moved the data fetch to new component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import axios from 'axios';
-
+import Photo from "./Photo";
 
 function App() {
-  const [nasaImg, setImg] = useState(0);
-  
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const nasaData = await axios.get('https://api.nasa.gov/planetary/apod?api_key=NAiBVbBWUEMwQapNNOSb7drcvy0f0vTdYpF1aRmP')
-        console.log(nasaData);
-        setImg(nasaData.data);
-      }
-        catch (err){
-          console.log(err);
-        }
-      }
-      fetchData()
-    },[])
-    console.log(nasaImg);
-
   return (
-    <div className="App">
-      <h2>{nasaImg.title}</h2>
-      <h2>{nasaImg.date}</h2>
-      <p>{nasaImg.explanation}</p>
-      <img src={`${nasaImg.url}`} />
-    </div>
+    <div className="container">
+            <h1>Astronomy Picture of the Day</h1>
+            <p>
+            <a href="https://apod.nasa.gov/apod/archivepix.html">Discover the cosmos!</a>
+            Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.
+            </p>
+            <Photo />
+            </div>
+
+
   );
 }
 
