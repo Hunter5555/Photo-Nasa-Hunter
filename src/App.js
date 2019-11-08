ec501682990743a9e6822cd5eb93a9ec11cebd4f
Hunter5555 <hunter.m.allan@gmail.com>
2019-11-07 17:21:15 -0700

    styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Photo from "./Photo";
+import styled from "styled-components";
 
-function App() {
+
+const Container = styled.div`
+text-align: center;
+max-width: 1000px;
+width: 80%;
+margin: 0 auto;
+background-color: teal;
+border-radius: 1rem;
+padding: 3rem;
+box-shadow: 3px 10px 10px rgba(0, 0, 0, 0.5);
+;`
+const App = () => {
   return (
-    <div className="container">
-            <h1>Astronomy Picture of the Day</h1>
-            <p>
-            <a href="https://apod.nasa.gov/apod/archivepix.html">Discover the cosmos!</a>
-            Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.
-            </p>
-            <Photo />
-            </div>
+      <Container>
+          <h1>Astronomy Picture of the Day</h1>
+          <p>
+              <a href="https://apod.nasa.gov/apod/archivepix.html">Discover the cosmos!</a>
+              Each day a different image or photograph of our fascinating universe is featured, along with a brief
+              explanation written by a professional astronomer.
+          </p>
 
-
+          <Photo />
+      </Container>
   );
-}
-
+};
 export default App;
